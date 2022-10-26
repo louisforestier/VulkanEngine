4aea0617f43598aa24cff7b88da9f70797bbb2a5
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2022-10-26 22:53:01 +0200

    Add vulkan-guide and change cmake to work on Linux

---
+++ vulkan-guide/src/CMakeFiles/vulkan_guide.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/louisf/Documents/VulkanProject/vulkan-guide/src/main.cpp" "src/CMakeFiles/vulkan_guide.dir/main.cpp.o" "gcc" "src/CMakeFiles/vulkan_guide.dir/main.cpp.o.d"
+  "/home/louisf/Documents/VulkanProject/vulkan-guide/src/vk_engine.cpp" "src/CMakeFiles/vulkan_guide.dir/vk_engine.cpp.o" "gcc" "src/CMakeFiles/vulkan_guide.dir/vk_engine.cpp.o.d"
+  "/home/louisf/Documents/VulkanProject/vulkan-guide/src/vk_initializers.cpp" "src/CMakeFiles/vulkan_guide.dir/vk_initializers.cpp.o" "gcc" "src/CMakeFiles/vulkan_guide.dir/vk_initializers.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/louisf/Documents/VulkanProject/vulkan-guide/third_party/CMakeFiles/vkbootstrap.dir/DependInfo.cmake"
+  "/home/louisf/Documents/VulkanProject/vulkan-guide/third_party/CMakeFiles/tinyobjloader.dir/DependInfo.cmake"
+  "/home/louisf/Documents/VulkanProject/vulkan-guide/third_party/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
