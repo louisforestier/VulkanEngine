649cd9f314591931871c8277474d7db0b2fecf4a
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2022-10-27 21:21:32 +0200

    Chapter 1 finished: initialization and render loop

--- vulkan-guide/src/vk_engine.h
+++ vulkan-guide/src/vk_engine.h
@@ -4,9 +4,11 @@
 #pragma once
 
 #include <vk_types.h>
+#include <vector>
 
 class VulkanEngine {
 public:
+
 
 	bool _isInitialized{ false };
 	int _frameNumber {0};
@@ -26,4 +28,41 @@ public:
 
 	//run main loop
 	void run();
+
+	VkInstance _instance; //Vulkan context handle
+	VkDebugUtilsMessengerEXT _debug_messenger; //Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; //GPU chosen as the default device
+	VkDevice _device ;//Vulkan device for commands
+	VkSurfaceKHR _surface; // Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat; // Image format expected by the windowing system
+	std::vector<VkImage> _swapchainImages; // Array of images from the swapchain
+	std::vector<VkImageView> _swapchainImageViews; // Array of image-views from the swapchain
+
+	VkQueue _graphicsQueue; // queue we will submit to
+	uint32_t _graphicsQueueFamily; // family of that queue
+
+	VkCommandPool _commandPool; // Pool containing our commands
+	VkCommandBuffer _mainCommandBuffer; // the buffer we will record into
+
+	VkRenderPass _renderPass;
+	std::vector<VkFramebuffer> _framebuffers;
+
+	VkSemaphore _presentSemaphore, _renderSemaphore;
+	VkFence _renderFence;
+
+private:
+
+	void init_vulkan();
+
+	void init_swapchain();
+
+	void init_commands();
+
+	void init_default_renderpass();
+
+	void init_framebuffers();
+
+	void init_sync_structures();
 };
